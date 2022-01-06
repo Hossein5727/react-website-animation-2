f9179771c612cda85b744de136a96f1c3c6e000a
HosseinGhiasi <hosseinghiasi5727@gmail.com>
2022-01-06 19:12:43 +0330

    fix bog nav

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -11,7 +11,7 @@ function Layout({ children }) {
     }
 
     return (
-        <div>
+        <div className='overflow-x-hidden'>
             <DropDown
                 isOpen={isOpen}
                 toggle={toggle}
